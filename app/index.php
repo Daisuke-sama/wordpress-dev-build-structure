e0e12bab349434034741fcc1bc3470b8e95dba7a
Pavel Burylichau <pavel_burylichau@epam.com>
2018-02-26 15:08:32 +0300

    1st commit. Added structure of basic wp with separated code and development.

---
+++ app/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel_Burylichau
+ * Date: 26-Feb-18
+ * Time: 12:53 PM
+ */
+
+define( 'WP_USE_THEMES', true );
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
